f1a4ef582dd66f0fba6993088b0808414bcc0142
Christophe Virot <christophe.virot@gmail.com>
2017-08-10 22:35:20 +0200

    Génération de la classe Admin de NommenclatureJeu
    Génération de la classe Admin de NommenclatureJeu à l'aide de la commande "php app/console sonata:admin:generate" puis en spécifiant "Dglas\JeuBundle\Entity\NommenclatureJeu". Le choix "yes" pour la mise à jour du controller a été sélectionné.

---
+++ demasque_app/src/Dglas/JeuBundle/Admin/NommenclatureJeuAdmin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Dglas\JeuBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class NommenclatureJeuAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('nom')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('nom')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                ),
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('nom')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('nom')
+        ;
+    }
+}
---
+++ demasque_app/src/Dglas/JeuBundle/Controller/NommenclatureJeuAdminController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dglas\JeuBundle\Controller;
+
+use Sonata\AdminBundle\Controller\CRUDController;
+
+class NommenclatureJeuAdminController extends CRUDController
+{
+}

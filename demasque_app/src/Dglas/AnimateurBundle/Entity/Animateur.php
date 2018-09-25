99e930fec970e23505900c2ec568f6d70a2f3515
Christophe Virot <christophe.virot@gmail.com>
2018-09-25 22:05:04 +0200

    Création de l'entity Animateur

---
+++ demasque_app/src/Dglas/AnimateurBundle/Entity/Animateur.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Dglas\AnimateurBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Animateur
+ *
+ * @ORM\Table(name="animateur")
+ * @ORM\Entity(repositoryClass="Dglas\AnimateurBundle\Repository\AnimateurRepository")
+ */
+class Animateur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_arrivee", type="datetime")
+     */
+    private $dateArrivee;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set dateArrivee
+     *
+     * @param \DateTime $dateArrivee
+     *
+     * @return Animateur
+     */
+    public function setDateArrivee($dateArrivee)
+    {
+        $this->dateArrivee = $dateArrivee;
+
+        return $this;
+    }
+
+    /**
+     * Get dateArrivee
+     *
+     * @return \DateTime
+     */
+    public function getDateArrivee()
+    {
+        return $this->dateArrivee;
+    }
+}
+
